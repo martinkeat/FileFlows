e6988a998ea6fe68b70481eec3328b68470b6686
reven <john@reven.nz>
2023-03-25 12:21:19 +1300

    FF-436 - changed ProcessingLibraries enum

--- Server/Upgrade/Upgrade_1_1_0.cs
+++ Server/Upgrade/Upgrade_1_1_0.cs
@@ -23,11 +23,11 @@ public class Upgrade_1_1_0
     {
         var manager = DbHelper.GetDbManager();
         string sql =
-            "update DbObject set Data = json_set(Data, '$.AllLibraries', @1) where Type = 'FileFlows.Shared.Models.ProcessingNode' and";
+            "update DbObject set Data = json_set(Data, '$.AllLibraries', @1) where Type = 'FileFlows.Shared.Models.ProcessingNode' and ";
         if(manager is SqliteDbManager)
             sql  += " json_extract([Data], '$.AllLibraries') = @0";
         else
-            sql  += " json_value([Data], '$.AllLibraries') = @0";
+            sql  += " json_value(Data, '$.AllLibraries') = @0";
             
         // in 1.1.0 we changed ProcessingLibraries so 0 is All and 1 is Only
         // this is so the default value of 0, means it will process all libraries and not need the user to edit the node
