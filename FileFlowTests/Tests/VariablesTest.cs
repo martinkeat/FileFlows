130863b7b4de91d561a1868cf591636f70fbcc8d
John Andrews <john@reven.co.nz>
2023-08-06 11:02:03 +1200

    FF-1053 - fixing variable replacement

--- FileFlowTests/Tests/VariablesTest.cs
+++ FileFlowTests/Tests/VariablesTest.cs
@@ -7,7 +7,7 @@
 public class VariablesTest
 {
     /// <summary>
-    /// Tests plex variables in folder names are not excaped
+    /// Tests plex variables in folder names are not escaped
     /// </summary>
     [TestMethod]
     public void PlexTests()
@@ -26,6 +26,19 @@ public class VariablesTest
         Assert.AreEqual("ShowName (2020) bobby", VariablesHelper.ReplaceVariables("ShowName (2020) {tmdb-123456}", variables, stripMissing: true));
         Assert.AreEqual("ShowName (2020) drake", VariablesHelper.ReplaceVariables("ShowName (2020) {tvdb-123456}", variables, stripMissing: true));
         Assert.AreEqual("ShowName (2020) iceman", VariablesHelper.ReplaceVariables("ShowName (2020) {imdb-123456}", variables, stripMissing: true));
+    }
+
+    /// <summary>
+    /// Tests a variable with odd characters in it
+    /// </summary>
+    [TestMethod]
+    public void NotValidVariable()
+    {
+        var variables = new Dictionary<string, object>();
+        const string testString = "Test {thi$ is n0t a valid-variable{na-me}!}";
+        Assert.AreEqual(testString, VariablesHelper.ReplaceVariables(testString, variables, stripMissing: true));
+        variables.Add("thi$ is n0t a valid-variable{na-me}!", "odd{repl@ce}ment");
+        Assert.AreEqual("Test odd{repl@ce}ment", VariablesHelper.ReplaceVariables(testString, variables, stripMissing: true));
     }
     
 }
