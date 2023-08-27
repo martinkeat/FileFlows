0e1e97cb2e9469b85261b7771f21b7ca37cb5cdc
John Andrews <john@reven.co.nz>
2023-08-28 10:12:55 +1200

    FF-1087 - added "Edit Script" to "Tasks" page

--- Client/Pages/Scripts/Scripts.razor.editor.cs
+++ Client/Pages/Scripts/Scripts.razor.editor.cs
@@ -1,5 +1,6 @@
 using FileFlows.Client.Components;
 using FileFlows.Client.Components.Inputs;
+using FileFlows.Client.Components.ScriptEditor;
 using FileFlows.Plugin;
 using FileFlows.Shared.Validators;
 
@@ -7,7 +8,10 @@ namespace FileFlows.Client.Pages;
 
 public partial class Scripts
 {
-    private FileFlows.Client.Components.Dialogs.ImportScript ScriptImporter;
+    /// <summary>
+    /// The script importer
+    /// </summary>
+    private Components.Dialogs.ImportScript ScriptImporter;
 
     /// <summary>
     /// Editor for a Script
@@ -17,76 +21,9 @@ public partial class Scripts
     public override async Task<bool> Edit(Script item)
     {
         this.EditingItem = item;
-
-        List<ElementField> fields = new List<ElementField>();
-        bool flowScript = item.Type == ScriptType.Flow;
-
-        if (string.IsNullOrEmpty(item.Code))
-        {
-            item.Code = flowScript ? @"
-/**
- * Description of this script
- * @param {int} NumberParameter Description of this input
- * @output Description of output 1
- * @output Description of output 2
- */
-function Script(NumberParameter)
-{
-    return 1;
-}
-" : @"
-import { FileFlowsApi } from 'Shared/FileFlowsApi';
-
-let ffApi = new FileFlowsApi();
-";
-        }
-
-        item.Code = item.Code.Replace("\r\n", "\n").Trim();
-
-        bool readOnly = item.Repository || item.Type == ScriptType.Shared;
-        string title = "Pages.Script.Title";
-
-        if (readOnly)
-        {
-            title = Translater.Instant("Pages.Script.Title") + ": " + item.Name;
-        }
-        else
-        {
-            fields.Add(new ElementField
-            {
-                InputType = FormInputType.Text,
-                Name = nameof(item.Name),
-                Validators = flowScript ? new List<FileFlows.Shared.Validators.Validator>
-                {
-                    new FileFlows.Shared.Validators.Required()
-                } : new ()
-            });
-        }
-
-
-        fields.Add(new ElementField
-        {
-            InputType = FormInputType.Code,
-            Name = "Code",
-            Validators = item.Type == ScriptType.Flow ? new List<FileFlows.Shared.Validators.Validator>
-            {
-                new FileFlows.Shared.Validators.ScriptValidator()
-            } : new List<Validator>()
-        });
-
-        var result = await Editor.Open(new()
-        {
-            TypeName = "Pages.Script", Title = title, Fields = fields, Model = item, Large = true, ReadOnly = readOnly,
-            SaveCallback = Save, HelpUrl = "https://fileflows.com/docs/pages/scripts",
-            AdditionalButtons = new ActionButton[]
-            {
-                new ()
-                {
-                    Label = "Labels.Import", 
-                    Clicked = (sender, e) => OpenImport(sender, e)
-                }
-            }
-        });
+        
+        var editor = new ScriptEditor(Editor, ScriptImporter, saveCallback: Save);
+        await editor.Open(item);
 
         return false;
     }
