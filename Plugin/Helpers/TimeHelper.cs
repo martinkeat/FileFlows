7b506302e24daee3e77dc1b383118e94ce3398c9
John Andrews <john@reven.co.nz>
2024-01-25 13:10:47 +1300

    stats

--- Plugin/Helpers/TimeHelper.cs
+++ Plugin/Helpers/TimeHelper.cs
@@ -12,13 +12,11 @@ public class TimeHelper
     /// <returns>A human-readable representation of the <see cref="TimeSpan"/>.</returns>\
     public static string ToHumanReadableString(TimeSpan t)
     {
-        if (t.TotalSeconds <= 1)
-            return $@"{t:s\.ff} seconds";
-        if (t.TotalMinutes < 2)
+        if (t.TotalSeconds < 120) // Less than 2 minutes
             return $@"{t:%s} seconds";
-        if (t.TotalHours < 2)
+        if (t.TotalMinutes < 120) // Less than 2 hours
             return $@"{t:%m} minutes";
-        if (t.TotalDays <= 1)
+        if (t.TotalHours < 24) // Less than 1 day
             return $@"{t:%h} hours";
         return $@"{t:%d} days";
     }
