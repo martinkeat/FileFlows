e69daab9d4094b19fc87ce16dcb03ac052dec63f
John Andrews <john@reven.co.nz>
2024-03-01 19:26:14 +1300

    external app

--- Server/Helpers/FileHelper.cs
+++ Server/Helpers/FileHelper.cs
@@ -1,12 +1,25 @@
 ﻿namespace FileFlows.Server.Helpers;
 
+/// <summary>
+/// Helper for file operations
+/// </summary>
 public class FileHelper
 {
+    /// <summary>
+    /// Creates a directory if it does not exist
+    /// </summary>
+    /// <param name="directory">the directory</param>
+    /// <returns>true if the directory was created</returns>
     public static bool CreateDirectoryIfNotExists(string directory)
     {
         return Plugin.Helpers.FileHelper.CreateDirectoryIfNotExists(Logger.Instance, directory);
     }
 
+    /// <summary>
+    /// Moves a file 
+    /// </summary>
+    /// <param name="source">the source file to move</param>
+    /// <param name="destination">the destination to move to</param>
     public static void MoveFile(string source, string destination)
         => File.Move(source, destination);
     
