a95e23f545f84342862b44bed2acf61b2923c50b
John Andrews <john@reven.co.nz>
2024-03-12 12:03:08 +1300

    dal - heatmap

--- ServerShared/Models/StatisticModels/Heatmap.cs
+++ ServerShared/Models/StatisticModels/Heatmap.cs
@@ -23,26 +23,31 @@ public class Heatmap
         // ensure all 672 buckets are included
         for (int i = 0; i <= 672; i++)
             data.TryAdd(i, 0);
-        
-        // REFACTOR: re-look into this, convert this to local time from UTC
-
 
         Dictionary<string, HeatmapData> results = new();
         var days = new[] { "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN" };
         foreach(var day in days.Reverse())
             results.Add(day, new () { Day = day, Data = new () });
+
+        int offset = -96; // 0-96 is sunday, we want to start monday
         
-        for (int i = 0; i < 672; i++)
+        for (int index = 0; index < 672; index++)
         {
+            int i = index + offset;
+            if (i < 0)
+                i = 672 + i;
+            else if (i >= 672)
+                i -= 672;
+                
             string day = days[(int)Math.Floor(i / 96f)];
             int time = i % 96;
             int hour = (int)Math.Floor(time / 4f);
             string x = hour == 0 ? "12am" : hour == 12 ? "12pm" : hour > 12 ? (hour - 12) + "pm" : hour + "am";
             var existing = results[day].Data.FirstOrDefault(y => y.Time == x);
             if (existing != null)
-                existing.Total += data[i];
+                existing.Total += data[index];
             else
-                results[day].Data.Add(new () { Time = x, Total = data[i] });
+                results[day].Data.Add(new () { Time = x, Total = data[index] });
         }
 
         return results.Values.ToList();
