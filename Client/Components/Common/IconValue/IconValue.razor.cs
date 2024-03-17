2b29278a98c1e4eab6d9e2026c3fe1c0271448f6
John Andrews <john@reven.co.nz>
2024-03-17 19:07:49 +1300

    css changes

---
+++ Client/Components/Common/IconValue/IconValue.razor.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Components;
+
+namespace FileFlows.Client.Components.Common;
+
+/// <summary>
+/// Icon Value 
+/// </summary>
+public partial class IconValue : ComponentBase
+{
+    /// <summary>
+    /// Gets or sets the icon
+    /// </summary>
+    [Parameter] public string Icon { get; set; }
+    /// <summary>
+    /// Gets or sets the value
+    /// </summary>
+    [Parameter] public string Value { get; set; }
+
+    /// <summary>
+    /// Gets or sets the the color 
+    /// </summary>
+    [Parameter] public IconValueColor Color { get; set; }
+
+    /// <summary>
+    /// Gets or sets the click event
+    /// </summary>
+    [Parameter] public Action OnClick { get; set; }
+
+    /// <summary>
+    /// User clicked the icon label
+    /// </summary>
+    private void ClickPerformed()
+        => OnClick?.Invoke();
+}
+
+/// <summary>
+/// A color for an icon value
+/// </summary>
+public enum IconValueColor
+{
+    /// <summary>
+    /// Blue
+    /// </summary>
+    Blue,
+    /// <summary>
+    /// Green
+    /// </summary>
+    Green,
+    /// <summary>
+    /// Purple
+    /// </summary>
+    Purple,
+    /// <summary>
+    /// Pink
+    /// </summary>
+    Pink,
+    /// <summary>
+    /// Orange
+    /// </summary>
+    Orange,
+    /// <summary>
+    /// Red
+    /// </summary>
+    Red
+}
