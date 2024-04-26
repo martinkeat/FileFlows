24a9d26e23cd3495011edf08866617804432374c
John Andrews <john@reven.co.nz>
2024-04-27 11:37:35 +1200

    FF-1145: inputtextarea variables drop down

--- Plugin/Attributes/TextAreaAttribute.cs
+++ Plugin/Attributes/TextAreaAttribute.cs
@@ -1,10 +1,22 @@
-namespace FileFlows.Plugin.Attributes
-{
-    using System;
-    using System.Collections.Generic;
+namespace FileFlows.Plugin.Attributes;
 
-    public class TextAreaAttribute : FormInputAttribute
+/// <summary>
+/// TextArea attribute which adds a TextArea to an editor
+/// </summary>
+public class TextAreaAttribute : FormInputAttribute
+{
+    /// <summary>
+    /// Gets if variables should be shown
+    /// </summary>
+    public bool Variables { get; init; }
+
+    /// <summary>
+    /// Initializes a new instance of the text area attribute
+    /// </summary>
+    /// <param name="order">the order the input appears</param>
+    /// <param name="variables">if variables should be shown</param>
+    public TextAreaAttribute(int order, bool variables = false) : base(FormInputType.TextArea, order)
     {
-        public TextAreaAttribute(int order) : base(FormInputType.TextArea, order) { }
+        Variables = variables;
     }
 }
